f93ca7ed1b12a04cc3a9b287a9ff56e044a59e5d
TeLoardbruh <damnloardbruh@gmail.com>
2023-01-16 21:37:59 +0700

    enhance: side bar and carousel

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
-function toggleOpenSideBar() {
+function openNav() {
+  document.getElementById('mySidebar').style.width = '250px';
   document.getElementById('main').style.marginLeft = '250px';
-
-  document.getElementsByClassName('side-bar-logo-area')[0].style.width =
-    '300px';
 }
 
-function closeToggleSideBarFromOutSide(event) {
-  const getElement = document.getElementById('main');
-
-  if (getElement === event.target.parentNode) {
-    document.getElementById('main').style.marginLeft = '0px';
-
-    document.getElementsByClassName('side-bar-logo-area')[0].style.width =
-      '0px';
-  }
+function closeNav() {
+  document.getElementById('mySidebar').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
 }
-
-document.addEventListener('click', closeToggleSideBarFromOutSide);
